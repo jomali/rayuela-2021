bfe31df3f5650ec55f98a4b3aa19055e4dc18894
J. Francisco Martín <jfm.lisaso@gmail.com>
2021-04-17 14:07:38 +0200

    Añadido script de compilación e intérprete Quixe

--- src/compila.sh
+++ src/compila.sh
@@ -4,10 +4,12 @@
 # Script para compilar y ejecutar relatos interactivos programados en Inform 6.
 # Herramientas utilizadas:
 #
-#	<>	inform: Compilador Inform 6 
+#	<>	inform: Compilador Inform 6
+#	<>	game2js: Convierte ficheros Z-code/Glulx a JavaScript
 #-------------------------------------------------------------------------------
 
 inform_compiler=./inform
+game2js=./game2js.py
 
 inform_path=./libs
 
@@ -28,6 +30,8 @@ fi
 echo "============================================="
 echo "COMPILANDO PARA GLULX…"
 echo "---------------------------------------------"
-$inform_compiler +include_path=$inform_path -G $gameFile.inf ../$gameFile.ulx
+$inform_compiler +include_path=$inform_path -G $gameFile.inf $gameFile.ulx
+python $game2js --giload $gameFile.ulx > ../web/stories/$gameFile.ulx.js
+rm $gameFile.ulx
 
 exit 0;
